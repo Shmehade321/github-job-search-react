5c36a9c95202e6ae6b33f01af8b0e757b9678eb6
Mehade Hasan <hasanmehade300@gmail.com>
2021-01-29 01:51:12 -0500

    Completed github job search project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import useFetchJobs from "./useFetchJobs";
+import { Container, Spinner, Alert } from "react-bootstrap";
+import JobCard from "./JobCard";
+import JobPagination from "./JobPagination";
+import JobSearchForm from "./JobSearchForm";
 
 function App() {
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
+  const { jobs, loading, error, hasNexPage } = useFetchJobs(params, page);
+
+  const handleParamChange = (e) => {
+    const param = e.target.name;
+    const value = e.target.value;
+    setPage(1);
+    setParams((prevParams) => {
+      return { ...prevParams, [param]: value };
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="my-4">
+      <h1 className="mb-4">
+        <i className="fab fa-github"></i> Github Jobs
+      </h1>
+      <JobSearchForm params={params} onParamChange={handleParamChange} />
+      <JobPagination page={page} setPage={setPage} hasNexPage={hasNexPage} />
+      {loading && (
+        <div className="text-center">
+          <Spinner
+            animation="border"
+            style={{ width: "5rem", height: "5rem" }}
+            role="status"
+          >
+            <span className="sr-only">Loading...</span>
+          </Spinner>
+        </div>
+      )}
+      {error && (
+        <Alert variant="danger">
+          <strong>Error. Try Refreshing.</strong>
+        </Alert>
+      )}
+      {jobs.map((job) => (
+        <JobCard key={job.id} job={job} />
+      ))}
+      <JobPagination page={page} setPage={setPage} hasNexPage={hasNexPage} />
+    </Container>
   );
 }
 
